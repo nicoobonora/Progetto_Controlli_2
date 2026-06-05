8a9d8fbedcd87fcfa19146bdb219be610f9e6ee2
nicoobonora <nicobonoraaa@gmail.com>
2026-06-05 07:35:07 +0200

    Corrette le variabili (vedi commenti)

--- progetto.m
+++ progetto.m
@@ -9,12 +9,11 @@ Re = 4.8; % resistenza da scaldare
 Rth = 16.7; % resistenza termica della cella
 Tp = celsiusToKelvin(15); % temperatura cella
 Tamb = celsiusToKelvin(24); % temperatura ambiente
-Tdiff = celsiusToKelvin(20); % Temperatura di differenza (???)
+Tdiff = 20; % Temperatura di differenza (???) (Essendo una differenza va lasciata in K)
 S = pi*d*l; % superficie di interesse (cilindrica)
 h = 6.55; % coefficiente di convenzione
 Le = 0; % induttanza
 Tavg = celsiusToKelvin(70); % temperatura media
-l = 15; % lunghezza del filo
 dlmax = l/100 * 4; % dlmax
 r1 = 0.0045; % raggio interno della puleggia
 r2 = 0.045; % raggio interno della puleggia
@@ -25,19 +24,19 @@ g = 9.80665; % forza di gravità
 K_star = (m*g*r2)/(r1*(dlmax-(r1/r2)*z_star)); % costante
 c = 6.2; % altra costante dello SMA
 K_max = 3.92 * 10^3; % costante massima
-x2_eq = Tamb + (Tdiff/c)*(log(K_star/(K_max - K_star))); % temperatura per tenere il peso a 4cm
-dK_dx2 = ((c*K_max)/Tdiff)*(exp(c*(x2_eq-Tamb)/Tdiff)/(1+(exp(c*(x2_eq-Tamb)/Tdiff)^2))); % altro parametro per la matrice
+x2_eq = Tavg + (Tdiff/c)*(log(K_star/(K_max - K_star))); % temperatura per tenere il peso a 4cm
+dK_dx2 = (c*K_max/Tdiff)*exp(c*(x2_eq - Tavg)/Tdiff)/(1 + exp(c*(x2_eq - Tavg)/Tdiff))^2; % altro parametro per la matrice
 x1_eq = sqrt((1/Re)*(h*S*(x2_eq - Tamb)+(x2_eq-Tp)/Rth)); % Corrente equivalente 
 K = K_max*(1-(1/(1+exp(c*((x2_eq-Tavg)/Tdiff)))));
 
 % Matrici
 a11 = -((h*S+(1/Rth))/Cth)
-a31 = (r1/r2)*((dlmax-((r1/r2)*z_star))/(m+(J/((r2)^2))))
+a31 = (r1/r2)*((dlmax-(r1/r2)*z_star)/(m+(J/r2^2))) * dK_dx2;
 a32 = -((((r1)^2)*K)/(((r2)^2)*(m+(J/((r2)^2)))))
 a33 = -(beta/(m+(J/((r2)^2))))
 
 
-b11 = 2/Cth;
+b11 = 2*x1_eq/Cth;
 
 A = [a11, 0, 0; 0, 0, 1; a31, a32, a33]
 B = [b11; 0; 0];
@@ -53,13 +52,8 @@ G = tf(model);
 G
 bode(G);
 
-figure(2);
 % Retroaction
+figure(2);
 F = G/(1+G);
 bode(F)
 F
-
-figure(3);
-rlocus(F)
-figure(4);
-rlocus(G)
